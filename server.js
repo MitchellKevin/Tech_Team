055f9d613b6820136af052e67298cd207986d978
Mitchell Scholte <mitchell.scholte04@gmail.com>
2025-03-13 11:18:07 +0100

    update

--- server.js
+++ server.js
@@ -70,7 +70,7 @@ app.post("/signup", upload.single('avatar'), async (req, res, next) => {
     const newUser = {
       name: req.body.name,
       password: hashedPassword, // Sla de gehashte versie op!
-      avatar: req.file.avatar
+      avatar: req.file.filename,
     };
 
     await usersCollection.deleteMany({}); // Verwijder alle gebruikers (voor testdoeleinden, waarschijnlijk niet gewenst in productie)
@@ -117,61 +117,13 @@ app.post('/profile' , upload.single('avatar'), (req, res, next) => {
   // res.send('File uploaded');
 });
 
-app.post('/photos/upload', upload.array('photos', 12), (req, res, next) => {
-  console.log(req.files);
-  res.send('Files uploaded');
-});
 
-const cpUpload = upload.fields([{ name: 'avatar', maxCount: 1 }, { name: 'gallery', maxCount: 8 }]);
+const cpUpload = upload.fields([{ name: 'avatar', maxCount: 1 }]);
 app.post('/cool-profile', cpUpload, (req, res, next) => {
   console.log(req.files);
   console.log(req.body);
   res.send('Files uploaded');
 });
-
-// app.post('/login', async (req, res) => {
-//   try {
-//       const database = client.db(process.env.db_name);
-//       const usersCollection = database.collection('users'); 
-//       const user = await usersCollection.findOne({
-//           name: req.body.name
-//       });
-//       if (user) {
-//           console.log('User found:', user);
-//           if (user.password === encryptWithSecretKey(req.body.password)) {
-//               console.log('User logged in:', user);
-//           } else {
-//               console.log('Invalid password');
-//           }
-//       }
-//   } catch (error) {
-//       console.error('Error finding user:', error);
-//       res.status(500).send('Error finding user');
-//   }
-// });
-
-
-// async function findUser(userName) {
-//   try {
-//     const database = client.db(process.env.db_name);
-//     const usersCollection = database.collection('users');
-//     const user = await usersCollection.findOne({
-//       name: userName
-//     });
-//     console.log('User found:', user);
-//     return user;
-//   } catch (error) {
-//     console.error('Error finding user:', error);
-//   }
-// }
-
-
-// Encryption and decryption functions
-
-// async function hashPassword(text) {
-//   const saltRounds = 10;
-//   return await bcrypt.hash(text, saltRounds);
-// }
 
 
 // https://dev.to/shubhamkhan/beginners-guide-to-aes-encryption-and-decryption-in-javascript-using-cryptojs-592
@@ -199,43 +151,3 @@ const encryptWithSecretKey = (text) => {
 
   return encryptedBase64;
 };
-
-
-// const decryptWithSecretKey = (encryptedText) => {
-//   try {
-//     const fullCipher = CryptoJS.enc.Base64.parse(encryptedText);
-
-//     // Extract IV and ciphertext from the parsed cipher
-//     const iv = CryptoJS.lib.WordArray.create(fullCipher.words.slice(0, 4), 16);
-//     const ciphertext = CryptoJS.lib.WordArray.create(fullCipher.words.slice(4));
-
-//     const cipherParams = CryptoJS.lib.CipherParams.create({
-//       ciphertext: ciphertext,
-//     });
-
-//     // Fetch and parse the secret key from environment variables
-//     const secretKey = process.env.security_key?.replace(
-//       /\\n/g,
-//       "\n"
-//     );
-
-//     // Decrypt the ciphertext using AES and the provided secret key
-//     const decrypted = CryptoJS.AES.decrypt(
-//       cipherParams,
-//       CryptoJS.enc.Hex.parse(secretKey),
-//       {
-//         iv: iv,
-//         padding: CryptoJS.pad.Pkcs7,
-//         mode: CryptoJS.mode.CBC,
-//       }
-//     );
-
-//     // Return decrypted text in UTF-8 format
-//     decrypted = decrypted.toString(CryptoJS.enc.Utf8);
-//     console.log("Decrypted text:", decrypted);
-//     return decrypted;
-//   } catch (error) {
-//     console.error("Decryption error:", error);
-//     return;
-//   }
-// };
