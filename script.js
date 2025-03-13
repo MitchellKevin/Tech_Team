02e4429cf346fe0dff3ffb8698d845f861236dca
kellyk15 <kellykha2004@gmail.com>
2025-03-13 10:53:43 +0100

    style en index

---
+++ script.js
@@ -0,0 +1,20 @@
+var deButton = document.querySelector(".menu");
+
+deButton.onclick = toggleMenu;
+
+function toggleMenu() {  
+  var deNav = document.querySelector("nav");
+  deNav.classList.toggle("toonMenu");
+}
+
+
+window.onkeydown = handleKeydown;
+
+function handleKeydown(event) {
+  if (event.key == "Escape") {
+    var deNav = document.querySelector("nav");
+    deNav.classList.remove("toonMenu");
+  }
+}
+
+console.log ("hoi");
