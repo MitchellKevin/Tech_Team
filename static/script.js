c7a856ba14c7cf6365f4b4d0cdf1634c21d83560
kellyk15 <kellykha2004@gmail.com>
2025-03-20 13:33:42 +0100

    homepage af behalve nav bar

---
+++ static/script.js
@@ -0,0 +1,58 @@
+var deButton = document.querySelector(".menu");
+
+deButton.onclick = toggleMenu;
+
+function toggleMenu() {  
+  var deNav = document.querySelector("nav");
+  deNav.classList.toggle("toonMenu");
+}
+
+
+window.onkeydown = handleKeydown;
+
+function handleKeydown(event) {
+  if (event.key == "Escape") {
+    var deNav = document.querySelector("nav");
+    deNav.classList.remove("toonMenu");
+  }
+}
+
+console.log ("hoi");
+
+
+// function clickHeart() {
+//   heart.classList.toggle("heartFilled");
+// }
+
+// liken
+// let heart = document.querySelector(".heartFill")
+// let heartstatus = true
+
+// function clickHeart(){
+//   if (heartstatus == true){
+//       heart.classList.add("heartFilled");
+//       heartstatus = false
+//   } else {
+//       heart.classList.remove("heartFilled");
+//       heartstatus = true
+//   }
+// }
+
+// heart.addEventListener('click',clickHeart)
+
+let hearts = document.querySelectorAll(".heartFill");
+let heartstatus = true;
+
+function clickHeart() {
+  if (heartstatus == true) {
+    this.classList.add("heartFilled");
+    heartstatus = false;
+  } else {
+    this.classList.remove("heartFilled");
+    heartstatus = true;
+  }
+}
+
+hearts.forEach(function(heart) {
+  heart.addEventListener('click', clickHeart);
+});
