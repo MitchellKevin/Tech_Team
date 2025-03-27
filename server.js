e51c58c3c8911abf6619a70f91872dc418c6ab62
Mitchell Scholte <mitchell.scholte04@gmail.com>
2025-03-27 14:49:07 +0100

    port 8000

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const express = require('express');
 const session = require('express-session');
 const cookieParser = require('cookie-parser');
 const app = express();
-const port = 8001;
+const port = 8000;
 app.use(express.static('static'));
 const CryptoJS = require("crypto-js");
 const bcrypt = require('bcrypt');
