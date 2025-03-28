e4faf0caa8ff9890f4f24f99bf3c3086b66a9396
kellyk15 <kellykha2004@gmail.com>
2025-03-28 23:34:11 +0100

    login en signup added

--- server.js
+++ server.js
@@ -56,11 +56,11 @@ app.get('/search', function(req, res) {
 });
 
 app.get('/login', function(req, res) {
-  res.render('logIn');
+  res.render('pages/logIn');
 });
 
 app.get('/signup', function(req, res) {
-    res.render('signUp');
+    res.render('pages/signUp');
 });
 
 app.get('/searchResult', function(req, res) {
