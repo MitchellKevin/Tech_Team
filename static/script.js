a4171609ab1128876892937d28e346633e12ba2a
kellyk15 <kellykha2004@gmail.com>
2025-03-31 15:40:38 +0200

    account af

--- static/script.js
+++ static/script.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 // var deButton = document.querySelector(".menu");
 
 // deButton.onclick = toggleMenu;
@@ -57,10 +56,10 @@ function clickHeart() {
 hearts.forEach(function(heart) {
   heart.addEventListener('click', clickHeart);
 });
-=======
+
 var deButton = document.querySelector(".menu");
 
-deButton.onclick = toggleMenu;
+// deButton.onclick = toggleMenu;
 
 function toggleMenu() {  
   var deNav = document.querySelector("nav");
@@ -78,4 +77,3 @@ function handleKeydown(event) {
 }
 
 console.log ("hoi");
->>>>>>> Account
