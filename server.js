acd46ac263cf52b69dbf9ac92e88b94a443a291c
Mitchell Scholte <mitchell.scholte04@gmail.com>
2025-03-31 16:29:56 +0200

    update fix

--- server.js
+++ server.js
@@ -235,7 +235,8 @@ app.post("/login", async (req, res) => {
       const token = jwt.sign({ name: user.name }, process.env.session_key, { expiresIn: "1h" });
       req.session.user = user;
       req.session.authenticated = true;
-      res.render("dashboard", { user: user });
+      res.cookie("session_id", req.sessionID, { maxAge: 1000 * 60 * 60 });
+      res.render("dashboard.ejs", { user: user });
       console.log(token);
     } else {
       console.log("Incorrect password");
